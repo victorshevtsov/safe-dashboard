8f5bc97480cff62d70cb26230626e8611bded02f
Victor Shevtsov <victorshevtsov@gmail.com>
2022-08-25 08:34:47 +0000

    Get rid of missing fonts in GlobalStyle.ts

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from 'styled-components'
-import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2'
-import avertaBoldFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2'
 
 const GlobalStyle = createGlobalStyle`
     html {
@@ -25,9 +23,7 @@ const GlobalStyle = createGlobalStyle`
 
     @font-face {
         font-family: 'Averta';
-        src: local('Averta'), local('Averta Bold'),
-        url(${avertaFont}) format('woff2'),
-        url(${avertaBoldFont}) format('woff');
+        src: local('Averta'), local('Averta Bold')
     }
 `
 
